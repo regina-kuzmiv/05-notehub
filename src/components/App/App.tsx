aee0cabf18a3143282fcc34215cbe118cd126054
regina <reginakuzmiv310@gmail.com>
2026-08-24 22:30:56 +0200

    added NoteList

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -10,3 +10,27 @@ export default function App() {}
   </header>
   <main>{/* Компонент NoteList */}</main>
 </div>;
+
+// export default function NoteList() {
+//   const [search, setSearch] = useState("");
+//   const [page, setPage] = useState(1);
+//   const { data, error, isSuccess, isLoading, isError } = useQuery({
+//     queryKey: ["note", search, page],
+//     queryFn: () => fetchNotes(search, page),
+//     enabled: search !== "",
+//   });
+//   return (
+//     <ul className={css.list}>
+//       <li className={css.listItem}>
+//         <h2 className={css.title}>Note title</h2>
+//         <p className={css.content}>Note content</p>
+//         <div className={css.footer}>
+//           <span className={css.tag}>Note tag</span>
+//           <button className={css.button} onClick={() => setPage(page + 1)}>
+//             Delete
+//           </button>
+//         </div>
+//       </li>
+//     </ul>
+//   );
+// }
--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,29 +1,34 @@
 import css from "./NoteList.module.css";
-import { useState } from "react";
-import fetchNotes from "../../services/noteService";
+import { useMutation } from "@tanstack/react-query";
+import { deleteNote } from "../../services/noteService";
+import type Note from "../../types/note";
 
-export default function NoteList() {
-    const [search, setSearch] = useState("");
-    const {
-      data,
-      error: isSuccess,
-      isLoading,
-      isError,
-    } = useQuery({
-      queryKey: ["note", search],
-      queryFn: () => fetchNotes(search),
-    });
-    return (
-        <ul className={css.list}>
-  <li className={css.listItem}>
-    <h2 className={css.title}>Note title</h2>
-    <p className={css.content}>Note content</p>
-    <div className={css.footer}>
-      <span className={css.tag}>Note tag</span>
-      <button className={css.button} onClick={()=>setSearch()}>Delete</button>
-    </div>
-  </li>
-</ul>;
-    )
+export default function NoteList({ notes }: { notes: Note[] }) {
+  const mutation = useMutation({
+    mutationFn: deleteNote,
+  });
+
+  const handleDeleteNote = (id: string) => {
+    mutation.mutate(id);
+  };
+
+  return (
+    <ul className={css.list}>
+      {notes.map((note) => (
+        <li key={note.id} className={css.listItem}>
+          <h2 className={css.title}>{note.title}</h2>
+          <p className={css.content}>{note.content}</p>
+          <div className={css.footer}>
+            <span className={css.tag}>{note.tag}</span>
+            <button
+              className={css.button}
+              onClick={() => handleDeleteNote(note.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 }
-
