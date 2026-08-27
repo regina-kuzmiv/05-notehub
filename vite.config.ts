0bcf20bdb2a67f48374aca32dc0b2f81d0c0da50
regina <reginakuzmiv310@gmail.com>
2026-08-27 16:19:37 +0200

    dfd

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/05-notehub/",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
